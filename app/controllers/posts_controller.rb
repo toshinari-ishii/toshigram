d81bd2b30b1a4fec2ff8f8a1961a45f3925763f9
toshinari-ishii <toshi1225m@gmail.com>
2020-01-23 16:35:50 +0900

    モデルの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
 
+  before_action :set_post, only: %i(show destroy)
+
   def new
     @post = Post.new
     @post.photos.build
@@ -21,8 +23,24 @@ class PostsController < ApplicationController
     @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
   end
 
+  def show
+  end
+
+  def destroy
+    if @post.user == current_user
+      flash[:notice] = "投稿が削除されました" if @post.destroy
+    else
+      flash[:alert] = "投稿の削除に失敗しました"
+    end
+    redirect_to root_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
+    end
+
+    def set_post
+      @post = Post.find_by(id: params[:id])
     end
 end
