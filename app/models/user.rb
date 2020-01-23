601fe1a594a7ae7a405928b23186fead006c354a
toshinari-ishii <toshi1225m@gmail.com>
2020-01-23 17:36:30 +0900

    いいね機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   
   has_many :posts, dependent: :destroy
+
+  has_many :likes
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
